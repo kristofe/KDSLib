d09c1e514169e98ceaa8d529899073e87fe1f4f4
Kristofer Schlachter <kristofe72@gmail.com>
2013-08-02 19:00:07 -0400

    Still working on Mesh and Vertex classes

---
+++ include/color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+#include "utils.h"
+
+struct Color
+{
+public:
+  Color();
+
+private:
+  //DISALLOW_COPY_AND_ASSIGN(Color);
+
+public:
+  float r,g,b,a;
+};
+
+#endif // COLOR_H
--- include/vertex.h
+++ include/vertex.h
@@ -1,34 +1,86 @@
 #ifndef VERTEX_H
 #define VERTEX_H
-#include "utils.h"
-#include "glm/glm.hpp"
+#include "glutil.h"
+#include <string>
+#include <vector>
 
-class Vertex
+struct VertexAttributeInfo
 {
-//Methods
+  public:
+    VertexAttributeInfo(const std::string pname,
+                        int pbuffer_offset,
+                        int pmemory_type,
+                        int pcount_of_memory_type
+                        ) :
+                          name(pname),
+                          buffer_offset(pbuffer_offset),
+                          memory_type(pmemory_type),
+                          count_of_memory_type(pcount_of_memory_type),
+                          location(0)
+    {
+    }
+
+  public:
+    std::string name;
+    int buffer_offset;
+    int memory_type;
+    int count_of_memory_type;
+    int location;
+};
+
+
+struct Vertex
+{
 public:
-   Vertex():_vertexStrideInFloats(16),_vertexStrideInBytes(16*sizeof(float))
-   {
+  static int get_position_offset(){ return BUFFER_OFFSET(0); }
+  static int get_normal_offset(){ return BUFFER_OFFSET(sizeof(Vector3)*1); }
+  static int get_uv_offset(){ return BUFFER_OFFSET(sizeof(Vector3)*2); }
+  static int get_uv2_offset(){ return BUFFER_OFFSET(sizeof(Vector3)*3); }
+  static int get_color_offset(){ return BUFFER_OFFSET(sizeof(Vector3)*4); }
 
-   }
+  static void GetAttributeInfo(std::vector<VertexAttributeInfo> *attributes)
+  {
+      attributes->push_back(VertexAttributeInfo("position",
+                                                get_position_offset(),
+                                                GL_FLOAT,
+                                                3)
+                            );
 
-   int getStrideInBytes(){ return sizeof(float)*16; }
+      attributes->push_back(VertexAttributeInfo("normal",
+                                                 get_normal_offset(),
+                                                 GL_FLOAT,
+                                                 3)
+                            );
+
+      attributes->push_back(VertexAttributeInfo("uv",
+                                                 get_uv_offset(),
+                                                 GL_FLOAT,
+                                                 3)
+                            );
+
+      attributes->push_back(VertexAttributeInfo("uv2",
+                                                 get_uv2_offset(),
+                                                 GL_FLOAT,
+                                                 3)
+                            );
+
+      attributes->push_back(VertexAttributeInfo("color",
+                                                 get_color_offset(),
+                                                 GL_FLOAT,
+                                                 3)
+                            );
+  }
 
 private:
-   DISALLOW_COPY_AND_ASSIGN(Vertex);
+  DISALLOW_COPY_AND_ASSIGN(Vertex);
 
 //Data Members
 public:
-   //TODO: Make sure glm::vec3 is actually using full floats.
-   glm::vec3 _position;
-   glm::vec3 _normal;
-   glm::vec3 _uv;
-   glm::vec3 _uv2;
-   glm::vec4 _color;
-
-
-
-
-
+  Vector3 position;
+  Vector3 normal;
+  Vector3 uv;
+  Vector3 uv2;
+  Color   color;
 };
+
 #endif // VERTEX_H
